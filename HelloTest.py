48a5c8cbd7e56dedd56ba0ff0cbf4956343baa65
kpesala146 <34053796+kpesala146@users.noreply.github.com>
2017-11-28 12:28:25 +0530

    Add files via upload

---
+++ HelloTest.py
@@ -0,0 +1,9 @@
+"Addition of two numbers"
+
+s1 = input("Please enter the number\n");
+s2 = input("Please enter second number");
+num1=int(s1);
+num2=int(s2);
+total = num1+num2;
+print(total);
+
---
+++ TestFunctions.py
@@ -0,0 +1,10 @@
+#How do use fuction
+#Fucntion definition here
+def printme(str)
+"This prints a passed string into this function"
+print(str)
+return;
+
+#Now you can call printme function
+printme(str="My String");
+printme("test");
